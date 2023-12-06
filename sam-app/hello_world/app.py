c814703bcdefd304463ff7c025faab65c8fd25dd
Kiranreddymule <rmule.my.bridgeport.edu>
2023-12-06 11:02:18 -0500

    text update

--- sam-app/hello_world/app.py
+++ sam-app/hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "kiran",
+            "message": "SRAVANI",
             # "location": ip.text.replace("\n", "")
         }),
     }
